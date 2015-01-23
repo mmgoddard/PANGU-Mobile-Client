58582253c9ca5846459024c314fcc8d726ad14c5
mmgoddard <mmgoddard@dundee.ac.uk>
2015-01-23 19:12:24 +0000

    Modified api so that it now took into account the change from test to pangu table in database.

--- PANGU-RESTful-API/src/main/java/com/controller/APIController.java
+++ PANGU-RESTful-API/src/main/java/com/controller/APIController.java
@@ -1,6 +1,6 @@
 package com.controller;
 
-import com.domain.TestModel;
+import com.domain.PanguModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +15,14 @@ import java.util.List;
  */
 @RequestMapping("/api/**")
 @RestController
-public class TestController {
-    private List<TestModel> list;
+public class APIController {
+    private List<PanguModel> list;
 
     @Autowired
     APIRepository apiRepository;
 
     @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<TestModel> index() {
+    public List<PanguModel> index() {
         return list = apiRepository.findAll();
     }
 }
--- PANGU-RESTful-API/src/main/java/com/service/APIRepository.java
+++ PANGU-RESTful-API/src/main/java/com/service/APIRepository.java
@@ -1,12 +1,12 @@
 package com.service;
 
-import com.domain.TestModel;
+import com.domain.PanguModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 /**
  * Created by Mark on 23/01/15.
  */
-public interface APIRepository extends JpaRepository<TestModel, Long> {
-    List<TestModel> findAll();
+public interface APIRepository extends JpaRepository<PanguModel, Long> {
+    List<PanguModel> findAll();
 }
