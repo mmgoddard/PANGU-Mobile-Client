ff801c7bd5d6b1e4adfebcc1059288eca8398b89
mmgoddard <mmgoddard@dundee.ac.uk>
2015-02-19 17:55:36 +0000

    Made changes so that the webapp uses controllers and appropriate routing.

--- v1-webapp/app/app.js
+++ v1-webapp/app/app.js
@@ -1,26 +1,21 @@
-/**
- * Created by Mark on 15/02/15.
- */
-'use strict';
-// Declare app level module which depends on views, and components
-var myapp = angular.module('myapp', [
-    'myappControllers',
-    'myapp.view',
-    'myapp.viewOne'
-]);
+var webApp = angular.module('web-app', ['ngRoute', 'webAppControllers']);
 
-myapp.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider.
-            when('/view', {
-                templateUrl: 'views/view.html',
-                controller: 'viewCtrl'
-            }).
-            when('/viewOne', {
-                templateUrl: 'views/viewOne.html',
-                controller: 'viewOneCtrl'
-            }).
-            otherwise({
-                redirectTo: '/index.html'
-            });
-    }]);
+//Configure our routes
+webApp.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+
+        //Route for the home page
+        .when('/', {
+            templateUrl : 'views/viewAll.html',
+            controller  : 'viewAllController'
+        })
+
+        // route for the about page
+        .when('/viewOne', {
+            templateUrl : 'views/viewOne.html',
+            controller  : 'viewOneController'
+        });
+
+    //Use the HTML5 History API
+    $locationProvider.html5Mode(true);
+});
--- v1-webapp/app/controllers.js
+++ v1-webapp/app/controllers.js
@@ -1,20 +1,21 @@
 /**
  * Created by Mark on 15/02/15.
  */
-var myappControllers = angular.module('myappControllers', []);
+var webAppControllers = angular.module('webAppControllers', []);
 
-myappControllers.controller('viewCtrl', ['$scope', '$http',
-    function ($scope, $http) {
+// create the controller and inject Angular's $scope
+webAppControllers.controller('viewAllController',
+    function($scope, $http) {
         $http.get('http://localhost:11000/api/models').
             success(function(data) {
                 $scope.pangu = data;
             });
-    }]);
+    });
 
-myappControllers.controller('viewOneCtrl', ['$scope', '$http',
-    function ($scope, $http) {
+webAppControllers.controller('viewOneController',
+    function($scope, $http) {
         $http.get('http://localhost:11000/api/models/3').
             success(function(data) {
                 $scope.pangu = data;
             });
-    }]);
+    });
