e999f12d6008e407063bab0326268987e8cd36f2
mmgoddard <mmgoddard@dundee.ac.uk>
2015-03-19 13:44:31 +0000

    Validation for both add/edit model pages.

--- v1-webapp/app/controllers.js
+++ v1-webapp/app/controllers.js
@@ -7,18 +7,18 @@ var webAppControllers = angular.module('webAppControllers', [])
 var addUpdateModelAddress = 'http://localhost:11000/api/models/add';
 var deleteViewModelAddress = 'http://localhost:11000/api/models/';
 
-webAppControllers.controller('viewModelsController',  [
+webAppControllers.controller('viewModelsController', [
     '$scope',
     '$http',
     '$location',
-    function($scope, $http, $location) {
+    function ($scope, $http, $location) {
         $http.get('http://localhost:11000/api/models').
-            success(function(data) {
+            success(function (data) {
                 $scope.pangu = data;
             });
 
-        $scope.setSelected = function(x) {
-            var url = '/models/'+x.id;
+        $scope.setSelected = function (x) {
+            var url = '/models/' + x.id;
             console.log(url);
             $location.path(url);
         };
@@ -29,33 +29,35 @@ webAppControllers.controller('editModelController', [
     '$http',
     '$routeParams',
     '$location',
-    function($scope, $http, $routeParams, $location) {
+    function ($scope, $http, $routeParams, $location) {
         var selectedId = $routeParams.id;
-        console.log("View Model: "+selectedId);
+        console.log("View Model: " + selectedId);
 
         //Get model based on ID
-        $http.get(deleteViewModelAddress+selectedId).
-            success(function(data) {
+        $http.get(deleteViewModelAddress + selectedId).
+            success(function (data) {
                 $scope.pangu = data;
             });
 
         //Update Model
-        $scope.updateModel = function(x) {
-            console.log("Update Model: "+ x.id)
-            $scope.json = angular.toJson(x);
-            $http.post(addUpdateModelAddress, $scope.json).
-                success(function (data, status, headers, config) {
+        $scope.updateModel = function (x) {
+            if ($scope.editModelForm.$valid) {
+                console.log("Update Model: " + x.id)
+                $scope.json = angular.toJson(x);
+                $http.post(addUpdateModelAddress, $scope.json).
+                    success(function (data, status, headers, config) {
 
-                }).
-                error(function (data, status, headers, config) {
+                    }).
+                    error(function (data, status, headers, config) {
 
-                });
+                    });
+            }
         };
 
         //Delete Model
-        $scope.deleteModel = function(id) {
-            console.log("Delete Model: "+id);
-            $http.delete(deleteViewModelAddress+id).
+        $scope.deleteModel = function (id) {
+            console.log("Delete Model: " + id);
+            $http.delete(deleteViewModelAddress + id).
                 success(function (data, status, headers, config) {
 
                 }).
@@ -70,19 +72,20 @@ webAppControllers.controller('addNewModelController', [
     '$scope',
     '$http',
     '$location',
-    function($scope, $http, $location) {
-        console.log("Add New Model");
+    function ($scope, $http, $location) {
+        $scope.addModel = function (panguRecord) {
+            if ($scope.addModelForm.$valid) {
+                console.log("Add New Model");
+                $scope.json = angular.toJson(panguRecord);
+                $http.post(addUpdateModelAddress, $scope.json).
+                    success(function (data, status, headers, config) {
 
-        $scope.addModel = function(panguRecord) {
-            $scope.json = angular.toJson(panguRecord);
-            $http.post(addUpdateModelAddress, $scope.json).
-                success(function (data, status, headers, config) {
+                    }).
+                    error(function (data, status, headers, config) {
 
-                }).
-                error(function (data, status, headers, config) {
-
-                });
-            $location.path("/models");
+                    });
+                $location.path("/models");
+            }
         }
     }]);
 
@@ -90,8 +93,8 @@ webAppControllers.controller('loginController', [
     '$scope',
     '$http',
     '$location',
-    function($scope, $http, $location) {
-        $scope.setLogin = function(login) {
+    function ($scope, $http, $location) {
+        $scope.setLogin = function (login) {
             if ($scope.loginForm.$valid) {
                 console.log("Login");
                 $location.path("/models");
